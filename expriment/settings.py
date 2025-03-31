f418a0bfe1226f965054940c305e3637b222b013
LinReseach <linlincheng.research@gmail.com>
2025-03-31 15:25:44 +0200

    contains parameters for ip, eye-tracking status, participant information. •	randomizer.py: shuffle trials.

---
+++ expriment/settings.py
@@ -0,0 +1,23 @@
+participant_id = 35
+
+# Participants 1 until and including 8 started with glasses.
+# 9 did NOT start with glasses but instead did the L2CS first.
+# From 17 onwards, L2CS first
+# 32 started with eyetracker
+
+
+ip = [
+    # TP-Link
+    '10.0.0.148', # 148 = Alan
+    '10.0.0.197', # 197 = Herbert ??
+    '10.0.0.198', # 196 = Herbert ?? - Latest
+    '10.0.0.165', # 165 = Marvin
+
+    # VU Research Device Net
+    '10.15.3.144', # 144 = Marvin
+    '10.15.2.168', # Herbert
+    '10.15.3.176'  # Alan
+    ][0]
+    
+has_eyetracker = False
+is_training = True
